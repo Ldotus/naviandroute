3e65eb3ca85a05c8c831b504adbdfdb65ca7e978
Liam <librok5420@gmail.com>
2023-07-28 02:18:03 +0100

    Before I remove navigator pop

--- lib/Pages/Routes.dart
+++ lib/Pages/Routes.dart
@@ -1,23 +1,26 @@
-
+// Importing necessary packages and files
 import 'package:naviandroute/Pages/Page1.dart';
-
-import 'Page2.dart';
+import 'Todo.dart';
 import 'Settings.dart';
-
 import 'package:flutter/material.dart';
 
+// Defining a class for managing routes and widget navigation
 class Routes {
+  // Defining route names as constants to avoid typos
   static const String settings = '/settings';
   static const String page1 = '/page1';
-  static const String page2 = '/page2';
+  static const String page2 = '/todo';
 
+  // A map of routes and their associated WidgetBuilders
   static Map<String, WidgetBuilder> routes = {
+    // Mapping the 'settings' route to the Settings page with the route name as the title
     settings: (context) => Settings(title: settings.toString()),
   };
 
+  // A static list of widgets representing different screens or UI components
   static const List<Widget> list = <Widget>[
-    Settings(title: "title"),
-    Page1(),
-    Page2(),
+    Settings(title: "title"), // A Settings widget with a title "title"
+    Page1(), // An instance of the Page1 widget
+    Todo(), // An instance of the Todo widget
   ];
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:naviandroute/Pages/Introscreen/ListPageViewlModel.dart';
 import 'package:naviandroute/Pages/Page1.dart';
 import 'package:naviandroute/Pages/Routes.dart';
 import 'package:naviandroute/Pages/Settings.dart';
 import 'package:naviandroute/widgets/redirectButton.dart';
-
-import 'Pages/Page2.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'Pages/Todo.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+// MyApp class - The root of the application
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      // Defining the theme for the application
       theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-          bottomNavigationBarTheme: BottomNavigationBarThemeData(
-              backgroundColor: Colors.indigo[400])),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          backgroundColor: Colors.indigo[400],
+        ),
+      ),
+      // Defining the routes for the application
       routes: Routes.routes,
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      // Setting the initial page for the application using listPageViewModel
+      home: ListPageViewModel().getPage(),
     );
   }
 }
 
+// MyHomePage class - A StatefulWidget with a counter
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -38,33 +46,45 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+// _MyHomePageState class - State class for MyHomePage
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   int selectedIndex = 0;
 
+  // Function to handle bottom navigation bar item taps
   void onItemTapped(int index) {
     setState(() {
       selectedIndex = index;
       switch (index) {
         case 0:
+          // Navigating to Settings page
           Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const Settings(title: "title"),
-              ));
+            context,
+            MaterialPageRoute(
+              builder: (context) => const Settings(title: "title"),
+            ),
+          );
           break;
         case 1:
+          // Navigating to Page1
+
           Navigator.push(
-              context, MaterialPageRoute(builder: (context) => const Page1()));
+            context,
+            MaterialPageRoute(builder: (context) => const Page1()),
+          );
           break;
         case 2:
+          // Navigating to Page2
           Navigator.push(
-              context, MaterialPageRoute(builder: (context) => const Page2()));
+            context,
+            MaterialPageRoute(builder: (context) => const Todo()),
+          );
           break;
       }
     });
   }
 
+  // Function to increment the counter
   void _incrementCounter() {
     setState(() {
       _counter++;
@@ -78,13 +98,16 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-
+      // Bottom Navigation Bar
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
+          // Settings icon and label
           BottomNavigationBarItem(
               icon: Icon(Icons.settings), label: "Settings"),
+          // Page 1 icon and label
           BottomNavigationBarItem(
               icon: Icon(Icons.last_page_rounded), label: "Page 1"),
+          // Page 2 icon and label
           BottomNavigationBarItem(
               icon: Icon(Icons.access_alarm_outlined), label: "Page 2"),
         ],
@@ -96,22 +119,24 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+            const Text('You have pushed the button this many times:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            // Custom RedirectButton widget
             const RedirectButton(title: "2"),
           ],
         ),
       ),
+      // Floating Action Button to pop the current route
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {
+          Navigator.pop(context);
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
