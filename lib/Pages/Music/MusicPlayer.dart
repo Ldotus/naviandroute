c116c7dad1dd28f5800a8f4b22859c0393e6cd86
Liam <librok5420@gmail.com>
2023-08-01 00:23:32 +0100

    Created class to build Music Player

--- lib/Pages/Music/MusicPlayer.dart
+++ lib/Pages/Music/MusicPlayer.dart
@@ -1,5 +1,14 @@
 //TODO: Implement Music player class
 //Using audio editing packages
-//advanced audio trimming 
+//advanced audio trimming
 /*Scaffold 
 */
+import 'package:flutter/material.dart';
+
+class MusicPlayer extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
